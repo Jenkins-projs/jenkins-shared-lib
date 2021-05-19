1e1172d321ba70bbb6907a2a3ad4cefa4aaae3b5
Pavan Nnd <46885224+pavan-nndn@users.noreply.github.com>
2021-05-19 14:20:24 +0530

    Update cleanWorkspace.groovy

--- vars/cleanWorkspace.groovy
+++ vars/cleanWorkspace.groovy
@@ -98,8 +98,8 @@ def cleanupNode(def ws){
         println padJob(".. $dir.name")+" job of type ${job}"
       }
     }else{
-	  //println padJob(".... $dir.name")+" ${trash} removed - Blue Ocean/Stale Ws"
-	  println padJob(".... $dir.name")+" removed - Blue Ocean/Stale Ws"
+	  println padJob(".... $dir.name")+" \u267B removed - Blue Ocean/Stale Ws"
+	  //println padJob(".... $dir.name")+" removed - Blue Ocean/Stale Ws"
      // dir.deleteRecursive()
     }
     println()
