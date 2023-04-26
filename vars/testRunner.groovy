631c6620a61aa1cd760216bc4b6a99ddb8ac906f
Pavan Nnd <46885224+pavan-nndn@users.noreply.github.com>
2023-04-26 17:33:48 +0530

    Create testRunner.groovy

---
+++ vars/testRunner.groovy
@@ -0,0 +1,44 @@
+def call(args = [:]) {
+   
+    def defaultArgs = [
+			userInputTimeout       : 60
+            additionalAemEnvs      : []
+    ]
+
+    args = defaultArgs + args
+
+    def defaultAemEnvs = [
+            AemEnv.DEV1,
+            AemEnv.DEV2,
+            AemEnv.DEV3
+    ]
+
+    def aemEnvs = defaultAemEnvs + args.additionalAemEnvs
+
+    pipeline {
+        agent  master
+        stages {
+            stage('test') {
+                options {
+                    timeout(time: args.userInputTimeout, unit: 'MINUTES')
+                }
+                input {
+                    message "Select a branch and AEM environment."
+                    ok "Confirm"
+                    parameters {
+                      string(name: 'TEST_BRANCH_INPUT', defaultValue : 'develop', description: 'Which branch do you want to test?')
+                      choice(name: 'AEM_SERVER_INPUT', choices: aemEnvs*.resourceName, description: 'Which server to test against?')
+                      string(name: 'CYPRESS_SPEC_NAME_INPUT', defaultValue: '', description: 'Provide path after integration folder in Cypress Directory.')
+                      }
+                }
+
+                steps {
+                    script {
+                        echo "Selected branch: ${env.TEST_BRANCH_INPUT}"
+                        echo "Selected server: ${env.AEM_SERVER_INPUT}"
+						}
+					}
+			}
+		}
+	}
+}
